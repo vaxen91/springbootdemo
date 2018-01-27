3d7fd0e06d0936a51844bb16c05a8f8855769f63
Mattia Pietropaolo <mattia.pietropaolo@connexo.it>
2018-01-27 20:13:38 +0100

    init

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@ComponentScan(value={"com.example"})
+public class DemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+	
+	
+	
+}
